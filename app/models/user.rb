6d905c76089b3e7e1f9ac1acff967e7f52ed58aa
valeria campilongo <cvaleriac@hotmail.it>
2019-03-21 21:03:23 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
   has_secure_password
   has_many :rides
   has_many :attractions, through: :rides
+
+  def mood
+        return "" if nausea.nil? or happiness.nil?
+        self.nausea > self.happiness ? "sad" : "happy"
+    end
+
 end
